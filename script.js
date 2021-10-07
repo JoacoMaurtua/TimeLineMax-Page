326183a31b7bf7e79490e0568b62fff84a53674b
JoacoMaurtua <joacomaurtua@gmail.com>
2021-10-06 23:17:37 -0500

    advanced animation use reverse()

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-document.getElementById('cta').addEventListener('click', function(){
 
-  TweenMax.to('.panel',1.5,{
+  const t1 = new TimelineMax({
+      paused: true
+  })
+
+
+  t1.to('.panel',1.5,{
     scaleY:1,
     height: '100vh',
     ease: Elastic.easeOut,
    
   });
 
-  TweenMax.to('#light',1,{
+  t1.to('#light',1,{
     opacity: 1,
     y:0,
-    delay:1,
     ease:Bounce.easeOut,    
-  });
+  }, 1);
 
-  TweenMax.to('p',1,{
+  t1.to('p',1,{
     opacity: 1,
     y:20,
-    delay:1.4
   });
 
-  TweenMax.to('.box',3,{
+  t1.to('.box',3,{
     opacity: 1,
     scale: 1,
     ease: Elastic.easeOut.config(1,0.3),
   
-  })
+  }, "-=3.5");
+
+  t1.reversed(true);
 
 
-
+document.getElementById('cta').addEventListener('click', function(){
+    t1.reversed(!t1.reversed());
+    if(t1.reversed()){
+      t1.reverse();
+    }  
+    else{
+      t1.play()
+    }
 })
+
